63a54f3a9b1a2342af4871542749209ec8ee6ed3
Thomas Gonet <gonet.thomas@gmail.com>
2014-02-04 19:38:42 +0100

    Modif

--- L3_TP4/src/ex6/testDataOutput.java
+++ L3_TP4/src/ex6/testDataOutput.java
@@ -1,9 +1,11 @@
 package ex6;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.GregorianCalendar;
 
@@ -19,7 +21,7 @@ public class testDataOutput {
 		
 		
 		try {
-			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("pers.xml"));
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("pers.ser"));
 			oos.writeObject(p1);
 			oos.flush();
 			oos.close();
@@ -29,10 +31,25 @@ public class testDataOutput {
 			e.printStackTrace();
 		}
 		
+		Personne p3 = null;
+		try {
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream("pers.ser"));
+			p3 = (Personne) in.readObject();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		Personne p2 = Personne.lireBinaire(f);
 		
 		
 		System.out.println(p2);
+		
+		System.out.println(p3);
 	}
 
 }
